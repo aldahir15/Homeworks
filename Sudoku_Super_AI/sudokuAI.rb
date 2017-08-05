09ae8210008e95af6b4e0bd30671c8740e035bd4
Jose Aldahir Moreno <jamoren1@uci.edu>
2017-08-05 15:46:31 -0700

    Finished Super AI for Sudoku

---
+++ Sudoku_Super_AI/sudokuAI.rb
@@ -0,0 +1,31 @@
+################
+# Sudoku Solver
+################
+require_relative 'board'
+
+def solver(board)
+  return board.render if board.solved?
+  positions = seek_empty_cell(board)
+  (1..9).each do |num|
+    new_board = board.dup
+    if board.is_valid?(positions[0],positions[1], num)
+      new_board[positions] = num
+      solution = solver(new_board)
+      return solution unless solution.nil?
+    end
+  end
+  nil
+end
+
+# Few helper methods
+def seek_empty_cell(board)
+    board.rows.each_index do |i|
+        board.rows[i].each_index do |j|
+            pos = [i, j]
+            return pos if board[pos].value == 0
+        end
+    end
+end
+
+dummy_board = Board.new
+solver(dummy_board)
--- eight_queens/eight_queens_puzzle.rb
+++ eight_queens/eight_queens_puzzle.rb
@@ -6,12 +6,12 @@ require_relative "board.rb"
     col = board.queen_count
     (0...(board.size)).each do |i|
         if board.is_pos_valid?(i, col)
-        new_board = board.dup # DUPLICATE IT!!!!!!!
+          new_board = board.dup # DUPLICATE IT!!!!!!!
                               # YOU'RE MODIFYING THE ORIGINAL WITH POSITIONS IF YOU DONT
-        new_board.place_queen(i,col)
-        sub_solution = solve(new_board)
-        return sub_solution unless sub_solution.nil?
-      end
+          new_board.place_queen(i,col)
+          sub_solution = solve(new_board)
+          return sub_solution unless sub_solution.nil?
+        end
     end
     nil
   end
