60c7d37ee98a5e4c7068cd1da5f8c50fd8d572b0
Jose Aldahir Moreno <jamoren1@uci.edu>
2017-08-15 21:22:47 -0700

    Finished Rails example

--- W3D3/homework/app/models/person.rb
+++ W3D3/homework/app/models/person.rb
@@ -1,4 +1,8 @@
 class Person < ApplicationRecord
-  validate :name, presence: true 
-  primary_key: :id
+  validates :name, presence: true
+  validates :house_id, presence: true
+  has_many :house,
+    primary_key: :id,
+    foreign_key: :house_id,
+    class_name: 'House'
 end
--- W3D3/homework/db/schema.rb
+++ W3D3/homework/db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170816035618) do
+ActiveRecord::Schema.define(version: 20170816040820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "houses", force: :cascade do |t|
+    t.string "address"
+  end
 
   create_table "people", force: :cascade do |t|
     t.string "name"
