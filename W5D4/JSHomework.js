9ad04bf5609a865bfefdd0c259ac8638c2040c93
Jose Aldahir Moreno <jamoren1@uci.edu>
2017-08-30 21:24:05 -0700

    Done

--- W5D4/JSHomework.js
+++ W5D4/JSHomework.js
@@ -92,3 +92,35 @@ function sumOfNPrimes(number) {
   }
   return count;
 }
+
+function titleize(names, callback) {
+  let titleized = names.map(name => `Mx. ${name} Jingleheimer Schmidt`);
+  callback(titleized);
+};
+
+function Elephant(name, height, tricks) {
+  this.name = name;
+  this.height = height;
+  this.tricks = tricks;
+}
+
+Elephant.prototype.trumpet = function () {
+  console.log(`${this.name} the elephant goes 'phrRRRRRRRRRRR!!!!!!!'`);
+};
+
+Elephant.prototype.grow = function () {
+  this.height = this.height + 12;
+};
+
+Elephant.prototype.addTrick = function (trick) {
+  this.tricks.push(trick);
+};
+
+Elephant.prototype.play = function () {
+  trickIndex = Math.floor(Math.random() * this.tricks.length);
+  console.log(`${this.name} is ${this.tricks[trickIndex]}!`);
+};
+
+Elephant.paradeHelper = function (elephant) {
+  console.log(`${elephant.name} is trotting by!`);
+};
