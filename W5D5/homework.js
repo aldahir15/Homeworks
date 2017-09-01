2c55e89dc835552b6813b36e4f40a74a2a94a9cb
Jose Aldahir Moreno <jamoren1@uci.edu>
2017-08-31 20:16:42 -0700

    Initialized W5D5 hw

---
+++ W5D5/homework.js
@@ -0,0 +1,16 @@
+// window.setTimeout(function () {
+//   alert('HAMMER TIME!');
+// }, 5000);
+//
+// function hammerTime (time) {
+//   window.setTimeout(function () {
+//     alert(`${time} is HAMMER TIME!`);
+//   });
+// }
+
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
